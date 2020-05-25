7a668ee301eb2398c4ba8124bae2981b95efb923
Chris Stayte <ctstayte@gmail.com>
2020-05-25 12:26:26 -0400

    Updates

---
+++ precache-manifest.c9d6bb8339ba115f7351078081dcfdbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd07b0a9be02fa3049365c04dbaa6ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0864696b0701595a13b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3f926949f2b0a9837d9f",
+    "url": "/static/css/main.75850aa1.chunk.css"
+  },
+  {
+    "revision": "c0864696b0701595a13b",
+    "url": "/static/js/2.29fd7369.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.29fd7369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f926949f2b0a9837d9f",
+    "url": "/static/js/main.23e5f0e4.chunk.js"
+  },
+  {
+    "revision": "b02519be8309a663c5f0",
+    "url": "/static/js/runtime-main.e209ea33.js"
+  }
+]);
